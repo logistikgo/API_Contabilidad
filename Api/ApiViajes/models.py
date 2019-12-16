bf7f7ef0a507e281d86b219e7479578751469b07
Daniel Torres <youkiddingd@gmail.com>
2019-12-16 14:04:14 -0600

    Cambio de modelos

--- Api/ApiViajes/models.py
+++ Api/ApiViajes/models.py
@@ -8,30 +8,43 @@ class PendientesEnviar(models.Model):
     FechaDescarga = models.DateTimeField()
     Moneda = models.CharField(max_length=10)
     #Costo = models.FloatField(default=0)
-    CostoSubtotal = models.DecimalField(default=0, max_digits=30, decimal_places=5)
-    CostoIVA = models.DecimalField(default=0, max_digits=30, decimal_places=5)
-    CostoRetencion = models.DecimalField(default=0, max_digits=30, decimal_places=5)
-    CostoTotal = models.DecimalField(default=0, max_digits=30, decimal_places=5)
     #Precio = models.FloatField(default=0)
-    PrecioSubtotal = models.DecimalField(default=0, max_digits=30, decimal_places=5)
-    PrecioIVA = models.DecimalField(default=0, max_digits=30, decimal_places=5)
-    PrecioRetencion = models.DecimalField(default=0, max_digits=30, decimal_places=5)
-    PrecioTotal = models.DecimalField(default=0, max_digits=30, decimal_places=5)
     Status = models.CharField(max_length=15)
-    IsFacturaCliente = models.BooleanField()
-    IsFacturaProveedor = models.BooleanField()
     IsEvidenciaFisica = models.BooleanField()
     IsEvidenciaDigital = models.BooleanField()
+    Proyecto = models.CharField(max_length=30)
     class Meta:
         db_table="PendientesEnviar"
 
 
+class Ext_PendienteEnviar_Costo(models.Model):
+    IDPendienteEnviar = models.OneToOneField(PendientesEnviar, on_delete=models.CASCADE, db_column = 'IDPendienteEnviar', primary_key=True)
+    CostoSubtotal = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    CostoIVA = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    CostoRetencion = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    CostoTotal = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    IsFacturaProveedor = models.BooleanField(default=False)
+    class Meta:
+        db_table="Ext_PendienteEnviar_Costo"
+
+
+class Ext_PendienteEnviar_Precio(models.Model):
+    IDPendienteEnviar = models.OneToOneField(PendientesEnviar, on_delete=models.CASCADE, db_column = 'IDPendienteEnviar', primary_key=True)
+    PrecioSubtotal = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    PrecioIVA = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    PrecioRetencion = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    PrecioTotal = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    PrecioServicios = models.DecimalField(default=0, max_digits=30, decimal_places=5)
+    IsFacturaCliente = models.BooleanField(default=False)
+    class Meta:
+        db_table="Ext_PendienteEnviar_Precio"
+
+
 class RelacionConceptoxProyecto(models.Model):
-    RelacionIDConceptoxProyecto = models.AutoField(primary_key=True)
+    IDRelacionConceptoxProyecto = models.AutoField(primary_key=True)
     IDPendienteEnviar = models.ForeignKey(PendientesEnviar, on_delete=models.CASCADE, db_column = 'IDPendienteEnviar')
     IDConcepto = models.IntegerField(default=0)
     IDCliente = models.IntegerField(default=0)
     IDProveedor = models.IntegerField(default=0)
-    Proyecto = models.CharField(max_length=30)
     class Meta:
         db_table="RelacionConceptoxProyecto"
--- Api/ApiViajes/views.py
+++ Api/ApiViajes/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import PendientesEnviar, RelacionConceptoxProyecto
+from .models import PendientesEnviar, RelacionConceptoxProyecto, Ext_PendienteEnviar_Costo, Ext_PendienteEnviar_Precio
 from .serializers import PendientesEnviarSerializer
 
 class PendientesEnviarList(APIView):
@@ -14,11 +14,19 @@ class PendientesEnviarList(APIView):
 
     def post(self, request):
         data = JSONParser().parse(request)
+        if "NombreCortoCliente" not in data and "NombreCortoProveedor" not in data:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         serializer = PendientesEnviarSerializer(data=data)
         if serializer.is_valid():
             GetIDPendienteEnviar = serializer.save()
             GetDataRelacionxProyecto = RelacionConceptoxProyecto(IDConcepto= data["IDConcepto"], IDPendienteEnviar_id= GetIDPendienteEnviar.IDPendienteEnviar, IDCliente= data["IDCliente"], IDProveedor= data["IDProveedor"], Proyecto= data["Proyecto"])
             GetDataRelacionxProyecto.save()
+            if "NombreCortoCliente" in data:
+                NewExtCliente = Ext_PendienteEnviar_Precio(IDPendienteEnviar = GetIDPendienteEnviar, PrecioSubtotal = data["PrecioSubtotal"], PrecioIVA = data["PrecioIVA"], PrecioRetencion = data["PrecioRetencion"], PrecioTotal = data["PrecioTotal"])
+                NewExtCliente.save()
+            elif "NombreCortoProveedor" in data:
+                NewExtProveedor = Ext_PendienteEnviar_Costo(IDPendienteEnviar = GetIDPendienteEnviar, CostoSubtotal = data["CostoSubtotal"], CostoIVA = data["CostoIVA"], CostoRetencion = data["CostoRetencion"], CostoTotal = data["CostoTotal"])
+                NewExtProveedor.save()
             return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
