123df00aa1fda0905d19744235047270b704f11b
Daniel Torres <youkiddingd@gmail.com>
2020-01-08 09:59:18 -0600

    fix

--- Api/ApiViajes/urls.py
+++ Api/ApiViajes/urls.py
@@ -3,6 +3,6 @@ from ApiViajes import views
 
 urlpatterns = [
     url(r'api/$', views.PendientesEnviarList.as_view()),
-    url(r'^api/(?P<pk>.+_*.*)/$', views.PendientesEnviarUpdate.as_view()),
+    url(r'^api/(?P<pk>.+)/$', views.PendientesEnviarUpdate.as_view()),
 
 ]
