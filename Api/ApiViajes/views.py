bfb7035613058efdcc0359a5fc24a73615263188
Daniel Torres <youkiddingd@gmail.com>
2020-02-28 12:32:40 -0600

    up

--- Api/ApiViajes/views.py
+++ Api/ApiViajes/views.py
@@ -9,13 +9,17 @@ import json
 
 class PendientesEnviarList(APIView):
 
+	#Trae todos los registros de la base de datos en la tabla PendientesEnviar
     def get(self, request):
         GetPendientesenviar = PendientesEnviar.objects.all()
         serializer = PendientesEnviarSerializer(GetPendientesenviar, many=True)
         return Response(serializer.data)
 
+    #Crea el registro en la base de datos, en las tablas RelacionConceptoxProyecto, Ext_PendientEnviar_Costo (Proveedor), Ext_PrendienteEnviar_Precio (Cliente)
+    # y el registro principal de PendientesEnviar
     def post(self, request):
         ArrConceptos = JSONParser().parse(request)
+        #Verifica si se creara solamente 1 registro o se le paso un arreglo de viajes.
         if not isinstance(ArrConceptos, list):
             Aux = list()
             Aux.append(ArrConceptos)
@@ -25,12 +29,15 @@ class PendientesEnviarList(APIView):
         try:
             for data in ArrConceptos:
                 serializer = PendientesEnviarSerializer(data=data)
-                breakpoint()
+                #Verifica que la informacion proporcionada coincida con la estructura de la tabla de PendientesEnviar, y verifica que por lo menos
+                #se realizara factura para Cliente o Proveedor
                 if serializer.is_valid() and (data["IsFacturaCliente"] or data["IsFacturaProveedor"]):
                     GetIDPendienteEnviar = serializer.save()
+                    #Crea el registro de la tabla RelacionConceptoxProyecto
                     GetDataRelacionxProyecto = RelacionConceptoxProyecto(IDConcepto = data["IDConcepto"], IDPendienteEnviar_id= GetIDPendienteEnviar.IDPendienteEnviar, IDCliente= data["IDCliente"], IDProveedor= data["IDProveedor"])
                     GetDataRelacionxProyecto.save()
                     if data["IsFacturaCliente"]:
+                    	#Crea el registro de la tabla Ext_PendienteEnviar_Precio para la informacion de cobro
                         NewExtCliente = Ext_PendienteEnviar_Precio(IDPendienteEnviar = GetIDPendienteEnviar, PrecioSubtotal = data["PrecioSubtotal"], PrecioIVA = data["PrecioIVA"], PrecioRetencion = data["PrecioRetencion"], PrecioTotal = data["PrecioTotal"])
                         if "ServiciosTotal" in data:
                             NewExtCliente.ServiciosIVA = data["ServiciosIVA"]
@@ -39,11 +46,13 @@ class PendientesEnviarList(APIView):
                             NewExtCliente.ServiciosTotal = data["ServiciosTotal"]
                         NewExtCliente.save()
                     if data["IsFacturaProveedor"]:
+                    	#Crea el registro de la tabla Ext_PendienteEnviar_Costo para la informacion de Pago
                         NewExtProveedor = Ext_PendienteEnviar_Costo(IDPendienteEnviar = GetIDPendienteEnviar, CostoSubtotal = data["CostoSubtotal"], CostoIVA = data["CostoIVA"], CostoRetencion = data["CostoRetencion"], CostoTotal = data["CostoTotal"])
                         NewExtProveedor.save()
                 else:
                     raise Exception("Datos incorrectos")
         except:
+        	#Si hay algun error, se realiza un rollback para deshacer los cambios y se lanza un error 400
             transaction.savepoint_rollback(sid)
             transaction.set_autocommit(True)
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -51,6 +60,7 @@ class PendientesEnviarList(APIView):
         transaction.set_autocommit(True)
         return Response(GetIDPendienteEnviar.IDPendienteEnviar, status=status.HTTP_201_CREATED)
 
+    #Se recibe un arreglo de folios para la eliminacion de estos en la base de datos
     def delete(self, request):
         ArrFolios = JSONParser().parse(request)
         if not isinstance(ArrFolios, list):
@@ -66,6 +76,7 @@ class PendientesEnviarList(APIView):
         except PendientesEnviar.DoesNotExist:
             pass
         except:
+        	#Si hay algun error, se realiza un rollback para deshacer los cambios y se lanza un error 400
             transaction.savepoint_rollback(sid)
             transaction.set_autocommit(True)
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -77,6 +88,7 @@ class PendientesEnviarList(APIView):
 
 
 class PendientesEnviarUpdate(APIView):
+	#Trae la informacion del registro que coincide con el Folio proporcionado
     def get(self, request, pk):
         try:
             Folio = PendientesEnviar.objects.get(Folio=pk)
@@ -117,13 +129,17 @@ class PendientesEnviarUpdate(APIView):
             return Response(status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    #Actualiza el registro del Folio especificado con la informacion que viene en un JSON. Los nombres de los parametros tienen que 
+    #corresponder a los nombres de cada campo en las tablas
     def patch(self, request, pk):
     	try:
         	Folio = PendientesEnviar.objects.get(Folio=pk)
         except PendientesEnviar.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
+        #Se reemplaza la informacion de la tabla principal
         data = JSONParser().parse(request)
         serializer = PendientesEnviarSerializer(Folio, data=data, partial=True)
+        #Se reemplaza la informacion de la tabla de costos, utilizada para el pago
         if "CostoIVA" in data:
             Ext_Costo = Ext_PendienteEnviar_Costo.objects.get(IDPendienteEnviar = Folio.IDPendienteEnviar)
             Ext_Costo.CostoIVA = data["CostoIVA"]
@@ -131,6 +147,7 @@ class PendientesEnviarUpdate(APIView):
             Ext_Costo.CostoSubtotal = data["CostoSubtotal"]
             Ext_Costo.CostoTotal = data["CostoTotal"]
             Ext_Costo.save()
+        #Se reemplaza la informacion de la tabla de precios, utilizada para el cobro
         if "PrecioIVA" in data:
             Ext_Precio = Ext_PendienteEnviar_Precio.objects.get(IDPendienteEnviar = Folio.IDPendienteEnviar)
             Ext_Precio.PrecioIVA = data["PrecioIVA"]
@@ -148,6 +165,7 @@ class PendientesEnviarUpdate(APIView):
             return Response(status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    #Borra el registro del folio especificado en la URL
     def delete(self, request, pk):
         try:
             Folio = PendientesEnviar.objects.get(Folio=pk)
@@ -158,6 +176,8 @@ class PendientesEnviarUpdate(APIView):
 
 
 
+#Esta funcion sirve para traer la informacion de las tablas de Costos y precios para poder regresarla en la consulta de los registros,
+#de esta manera solo se tiene que consultar con 1 API para tener toda la infomacion.
 def PendienteEnviarToList(PendienteMain):
     try:
         Ext_Costo = Ext_PendienteEnviar_Costo.objects.get(IDPendienteEnviar = PendienteMain["IDPendienteEnviar"])
